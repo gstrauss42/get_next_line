e2e0a40ff0efe948f59b97b070a4d234b28bc795
Gabriel Strauss <gstrauss@c5r4s1.wethinkcode.co.za>
2019-06-17 08:37:27 +0200

    added stuff

---
+++ get_next_line.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gstrauss <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/14 09:41:18 by gstrauss          #+#    #+#             */
+/*   Updated: 2019/06/17 08:34:12 by gstrauss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		get_next_line(const int fd, char **line)
+{
+	t_list node;
+	int count;
+
+	while(buff[count] != '\n')
+	{
+		read(fd, buff, buf*sizeof char);
+		while(buff[count] != '\n' && buff[count])
+			count++;
+		if(buff[count] == '\n')
+		
