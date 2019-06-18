84f82b966a072f5d596b32489b8e3d52678f8e9a
Gabriel Strauss <gstrauss@c5r4s1.wethinkcode.co.za>
2019-06-18 09:34:07 +0200

    push

--- get_next_line.c
+++ get_next_line.c
@@ -6,45 +6,51 @@
 /*   By: gstrauss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/14 09:41:18 by gstrauss          #+#    #+#             */
-/*   Updated: 2019/06/17 15:49:16 by gstrauss         ###   ########.fr       */
+/*   Updated: 2019/06/18 08:57:35 by gstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 int		linecp(t_list **head, char **line)
 {
 	t_list *node;
 	int i;
 	int c;
+	static char *holder;
 
 	i = 0;
 	c = 0;
 	node = *head;
+	while(holder[i])
+	{
+		line[0][i] = holder[i];
+		i++;
+	}
 	while(node)
 	{
-		while(node.content[i] && node.content[i] != '\n')
+		while(node->content[i] && node->content[i] != '\n')
 		{
-			line[0][i] = node.content[c];
+			line[0][i] = node->content[c];
 			i++;
 			c++;
 		}
-		if(node.content[i] == '\n')
-			return(1);
-		node = node.next;
+		if(node->content[i] == '\n')
+			holder = ft_strchr(node->content, '\n') + 1;
+		node = node->next;
 		c = 0;
 	}
 	return(0);
 }
 
-int		reader(const int fd)
+int		reader(const int fd, char **line)
 {
 	int		i;
 	char	*buff;
 	t_list	**head;
 
 	i = 0;
-	buff = malloc
 	read(fd, buff, buff_size);
 	if(!ft_strchr(buff, '\n'))
 			head = &ft_lstnew(buff, buff_size);
@@ -58,5 +64,16 @@ int		reader(const int fd)
 
 int		get_next_line(const int fd, char **line)
 {
-	return(reader(fd));
+	return(reader(fd, line));
+}
+
+int main()
+{
+	size_t i;
+	i = 0;
+	int fd;
+	char **line;
+	line = NULL;
+	fd = open("text.txt", O_RDONLY);
+	printf("%d", get_next_line(fd, line));
 }
---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gstrauss <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/18 08:55:11 by gstrauss          #+#    #+#             */
+/*   Updated: 2019/06/18 09:33:50 by gstrauss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+#define BUFF_SIZE 2
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#endif
