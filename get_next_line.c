e07bc9d01675242f1ef83aefaae8cc3bc868a637
Gabriel Strauss <gstrauss@c5r1s5.wethinkcode.co.za>
2019-06-28 10:39:03 +0200

    did most of norm

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: gstrauss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/14 09:41:18 by gstrauss          #+#    #+#             */
-/*   Updated: 2019/06/28 10:25:11 by gstrauss         ###   ########.fr       */
+/*   Updated: 2019/06/28 10:38:04 by gstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 #include "libft.h"
 #include <stdio.h>
 
-char *readwrite(const int fd, char **line, char *buff, char *holder)
+char	*readwrite(const int fd, char **line, char *buff, char *holder)
 {
-	int count;
-	t_list **head;
-	t_list *node;
+	int		count;
+	t_list	**head;
+	t_list	*node;
 
 	count = 0;
-	while(read(fd, buff, BUFF_SIZE) > 0)
+	while (read(fd, buff, BUFF_SIZE) > 0)
 	{
 		buff[BUFF_SIZE + 1] = '\0';
-		if(count == 0)
+		if (count == 0)
 		{
 			node = ft_lstnew(buff, BUFF_SIZE + 1);
 			head = &node;
@@ -32,22 +32,24 @@ char *readwrite(const int fd, char **line, char *buff, char *holder)
 		else
 			ft_lstend(node, ft_lstnew(buff, BUFF_SIZE + 1));
 		count++;
-		if(ft_strchr(buff, '\n'))
-			break;
+		if (ft_strchr(buff, '\n'))
+			break ;
 	}
-	if(node && buff[0])
+	if (node && buff[0])
 	{
-		line[0] = (char *)malloc( count * BUFF_SIZE + ft_strnlen(buff, '\n') + 1 + ft_strlen(holder));
+		line[0] = (char *)malloc(count * BUFF_SIZE +
+				ft_strnlen(buff, '\n') + 1 + ft_strlen(holder));
 		ft_strcat(line[0], holder);
-		while(node->next)
+		while (node->next)
 		{
 			ft_strcat(line[0], node->content);
 			node = node->next;
 		}
-		line[0] = ft_strncat(line[0], node->content, ft_strnlen(node->content, '\n'));
-		ft_lstdel(head, ft_del);	
+		line[0] = ft_strncat(line[0], node->content
+				, ft_strnlen(node->content, '\n'));
+		ft_lstdel(head, ft_del);
 	}
-	return(buff);
+	return (buff);
 }
 
 char	*holder_cp(char *holder, char **line)
@@ -55,45 +57,45 @@ char	*holder_cp(char *holder, char **line)
 	int i;
 
 	i = 0;
-	while(holder[i] && holder[i] != '\n')
+	while (holder[i] && holder[i] != '\n')
 	{
 		line[0][i] = holder[i];
 		i++;
-		if(holder[i] == '\n')
+		if (holder[i] == '\n')
 			line[0][i] = '\0';
 	}
-	return(line[0]);
+	return (line[0]);
 }
 
 int		get_next_line(const int fd, char **line)
 {
-	char	*buff;
-	static char *holder;
-	int i;
-	int p;
-	char *tmp;
+	char		*buff;
+	static char	*holder;
+	int			i;
+	int			p;
+	char		*tmp;
+
 	line[0] = (char *)malloc(BUFF_SIZE);
+	tmp = (char *)malloc(BUFF_SIZE);
 	p = 0;
 	i = 0;
-	tmp = (char *)malloc(BUFF_SIZE);
-	if(!holder)
+	if (!holder)
 		holder = (char *)malloc(BUFF_SIZE);
 	buff = (char *)malloc(BUFF_SIZE + 1);
-	
-	if(holder[i])
+	if (holder[i])
 		line[0] = holder_cp(holder, line);
-	while(holder[i] && holder[i] != '\n')
+	while (holder[i] && holder[i] != '\n')
 		i++;
-	if(holder[i] == '\n')
+	if (holder[i] == '\n')
 		holder = ft_strcut(holder, '\n');
 	if (ft_strchr(holder, '\n') == NULL)
 	{
 		buff = readwrite(fd, line, buff, holder);
-		if(!buff[0])
+		if (!buff[0])
 		{
 			printf("%s\n", line[0]);
 			ft_bzero(holder, ft_strlen(holder));
-			return(0);
+			return (0);
 		}
 		buff = ft_strcut(buff, '\n');
 		ft_bzero(holder, BUFF_SIZE);
@@ -101,22 +103,23 @@ int		get_next_line(const int fd, char **line)
 	}
 	printf("%s\n", line[0]);
 	free(buff);
-	return(1);
+	return (1);
 }
-
+/*
 int main()
 {
-	int q;
-	int fd;
+	int			q;
+	int	fd;
 	char **line;
-	
+
 	q = 1;
 	line = (char **)malloc(1000);
 	fd = open("tester.txt", O_RDONLY);
-	while(q != 0)
+	while (q != 0)
 	{
 		q = get_next_line(fd, line);
 		printf("%d\n", q);
 	}
-	return(0);
+	return (0);
 }
+*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: gstrauss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/18 08:55:11 by gstrauss          #+#    #+#             */
-/*   Updated: 2019/06/28 10:14:44 by gstrauss         ###   ########.fr       */
+/*   Updated: 2019/06/28 10:33:28 by gstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
-#define BUFF_SIZE 5
+#define BUFF_SIZE 100 
 
 #include <sys/types.h>
 #include <sys/stat.h>
