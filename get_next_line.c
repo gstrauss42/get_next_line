87c331ab89056004ee3546bdbb0be53a14e95c05
Gabriel Strauss <gstrauss@c5r4s1.wethinkcode.co.za>
2019-07-02 13:41:04 +0200

    wow

--- get_next_line.c
+++ get_next_line.c
@@ -1,108 +1,64 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   gnltest.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gstrauss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/14 09:41:18 by gstrauss          #+#    #+#             */
-/*   Updated: 2019/07/02 09:59:14 by gstrauss         ###   ########.fr       */
+/*   Created: 2019/07/02 11:35:44 by gstrauss          #+#    #+#             */
+/*   Updated: 2019/07/02 13:38:40 by gstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft/libft.h"
 #include "get_next_line.h"
-#include "./libft/libft.h"
 #include <stdio.h>
 
-char	*readwrite(const int fd, char **line, char *buff, char *holder)
+int		gnltest(int fd, char **line)
 {
-	int		count;
-	t_list	**head;
-	t_list	*node;
+	int count;
+	char *buff;
+	static char *holder;
+	char *carry_over;
+	int red;
 
-	count = 0;
-	while (read(fd, buff, BUFF_SIZE) > 0)
-	{
-		buff[BUFF_SIZE + 1] = '\0';
-		if (count == 0)
-		{
-			node = ft_lstnew(buff, BUFF_SIZE + 1);
-			head = &node;
-		}
-		else
-			ft_lstend(node, ft_lstnew(buff, BUFF_SIZE + 1));
-		count++;
-		if (ft_strchr(buff, '\n'))
-			break ;
-	}
-	if (node && buff[0])
-	{
-		free(*line);
-		*line = (char *)malloc(count * BUFF_SIZE +
-				ft_strnlen(buff, '\n') + 1 + ft_strlen(holder));
-		ft_strcat(*line, holder);
-		while (node->next)
-		{
-			ft_strcat(*line, node->content);
-			node = node->next;
-		}
-		*line = ft_strncat(*line, node->content
-				, ft_strnlen(node->content, '\n'));
-		ft_lstdel(head, ft_del);
-	}
-	return (buff);
-}
-
-char	*holder_cp(char *holder, char *line)
-{
-	int i;
-
-	i = 0;
-	while (holder[i] && holder[i] != '\n')
-	{
-		line[i] = holder[i];
-		i++;
-		if (holder[i] == '\n')
-			line[i] = '\0';
-	}
-	return (line);
-}
-
-int		get_next_line(const int fd, char **line)
-{
-	char		*buff;
-	static char	*holder;
-	*line = (char *)malloc(BUFF_SIZE);
-	if (!holder)
-		holder = (char *)malloc(BUFF_SIZE);
+	if (!line || read(fd, NULL, 0) == -1)
+		return (-1);
 	buff = (char *)malloc(BUFF_SIZE + 1);
-	if (holder[0])
-		*line = holder_cp(holder, *line);
-	if(ft_strchr(holder, '\n'))
-		holder = ft_strcut(holder, '\n');
-	if (ft_strchr(holder, '\n') == NULL)
+	count = 0;
+	carry_over = NULL;
+	while((red = read(fd, buff, BUFF_SIZE)) > 0)
 	{
-		buff = readwrite(fd, line, buff, holder);
-		buff[ft_strlen(buff) + 1] = '\0';
-		if (!buff[0])
-		{
-			ft_bzero(holder, BUFF_SIZE);
-			free(buff);
-			free(holder);
-			return (0);
-		}
-		buff = ft_strcut(buff, '\n');
-		ft_bzero(holder, BUFF_SIZE);
-		ft_strcpy(holder, buff);
+		count++;
+		buff[red] = '\0';
+		holder = (char *)malloc(BUFF_SIZE * (count + 1));
+		holder[0] = '\0';
+		ft_strcat(holder, carry_over);
+		ft_strcat(holder, buff);
+		free(carry_over);
+		carry_over = holder;
+	}
+	holder[ft_strlen(holder) + 1] = '\0';
+	*line = (char *)malloc(ft_strnlen(holder, '\n') + 1);
+	ft_bzero(*line, ft_strlen(*line));
+	(*line)[ft_strnlen(holder, '\n') + 1] = '\0'; 
+	ft_strncat(*line, holder, ft_strnlen(holder, '\n'));
+	if (!ft_strcut(holder, '\n'))
+	{
+		free(holder);
+		free(buff);
+		return (0);
 	}
 	free(buff);
-	return (1);
+	if(!holder || !holder[0])
+		return(0);
+	return(1);
 }
 
 int main()
 {
-	int			q;
-	int	fd;
+	int q;
+	int fd;
 	int i;
 	char *line;
 
@@ -111,11 +67,10 @@ int main()
 	fd = open("./text_files/tester.txt", O_RDONLY);
 	while (q != 0)
 	{
-		q = get_next_line(fd, &line);
+		q = gnltest(fd, &line);
 		printf("%s", line);
+		ft_strdel(&line);
 		printf("%d\n", q);
-		i++;
 	}
-	sleep(10);
-	return (0);
+	return(0);
 }
