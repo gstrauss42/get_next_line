fde180fd46d249b855b0135702f77839a967ac95
Gabriel Strauss <gstrauss@c4r17s10.wethinkcode.co.za>
2019-07-04 11:12:45 +0200

    added stuff

--- get_next_line.c
+++ get_next_line.c
@@ -1,59 +1,81 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   gnltest.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: gstrauss <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/02 11:35:44 by gstrauss          #+#    #+#             */
-/*   Updated: 2019/07/03 07:38:59 by gstrauss         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft/libft.h"
 #include "get_next_line.h"
+#include <stdio.h>
 
-char	*linespace(int fd, char *holder)
+int	get_next_line(int fd, char **line)
 {
-	int			count;
-	char		*buff;
-	char		*carry_over;
-	int			red;
-
-	buff = (char *)malloc(BUFF_SIZE + 1);
-	count = 0;
-	carry_over = NULL;
-	while ((red = read(fd, buff, BUFF_SIZE)) > 0)
-	{
-		count++;
-		buff[red] = '\0';
-		holder = (char *)malloc(BUFF_SIZE * (count + 1));
-		holder[0] = '\0';
-		ft_strcat(holder, carry_over);
-		ft_strcat(holder, buff);
-		free(carry_over);
-		carry_over = holder;
-	}
-	holder[ft_strlen(holder) + 1] = '\0';
-	free(buff);
-	return (holder);
-}
-
-int		get_next_line(int fd, char **line)
-{
-	static char	*holder;
-
+	char *buff;
+	int check;
+	int count;
+	t_list *head;
+	
 	if (!line || read(fd, NULL, 0) == -1)
-		return (-1);
-	holder = linespace(fd, holder);
-	*line = (char *)malloc(ft_strnlen(holder, '\n') + 1);
-	ft_bzero(*line, ft_strlen(*line));
-	(*line)[ft_strnlen(holder, '\n') + 1] = '\0';
-	ft_strncat(*line, holder, ft_strnlen(holder, '\n'));
-	if (!ft_strcut(holder, '\n'))
+		return(-1);
+	*line = (char *)malloc(BUFF_SIZE + 2);
+	if(*line[0])
+		ft_bzero(*line, ft_strlen(*line));
+	count = 0;
+	buff = (char *)malloc(BUFF_SIZE + 1);
+	static char  *holder;
+	if(!holder)
+		holder = (char *)malloc(BUFF_SIZE + 1);
+	if(ft_strchr(holder, '\n'))
 	{
-		free(holder);
-		return (0);
+		ft_strncat(*line, holder, ft_strnlen(holder, '\n'));
+		(*line)[ft_strnlen(holder, '\n')] = '\0';
+		ft_strcut(holder, '\n');
 	}
-	return (1);
+	if(!ft_strchr(holder, '\n'))
+	{
+		while((check = read(fd, buff, BUFF_SIZE)) && check != 0)
+		{
+			buff[BUFF_SIZE] = '\0';
+			count++;
+			if(count == 1)
+				head = ft_lstnew(buff, BUFF_SIZE + 1);
+			else
+				ft_lstend(head, ft_lstnew(buff, BUFF_SIZE + 1));
+			if(ft_strchr(buff, '\n'))
+				break;
+		}
+		if(holder[0])
+			*line = ft_strjoin(*line, holder);
+		while(head->next)
+		{
+			ft_strcat(*line, head->content);
+			head = head->next;
+		}
+		*line = ft_strjoin(*line, head->content);
+		ft_nbzero(*line, ft_strnlen(*line, '\n'), ft_strlen(*line));
+		ft_bzero(holder, ft_strlen(holder));
+		holder = ft_strcut(buff, '\n');
+	}
+	free(buff);
+	if(!holder)
+		return(0);
+	return(1);
 }
+	
+int main()
+{
+	int fd;
+	int i;
+	char *line;
+	i = 1;
+	fd = open("texts.txt", O_RDONLY);
+	while(i != 0)
+	{
+		i  = get_next_line(fd, &line);
+		printf("%s\n", line);
+	}
+	return(0);
+}
+
+/*	
+	char * temp;
+	count = 1;
+	if(!holder)
+		while(case = read(fd, buff, BUFF_SIZE) && case != 0 && !ft_strchr(buff, '\n') && *line = (char *)malloc(BUFF_SIZE * 	count + 1) && ft_strcat(*line, buff) && (temp = *line) && free(temp))
+			count++;
+		ft_strncat(*line, buff, ft_strnlen(buff, '\n'));
+*/	
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: gstrauss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/18 08:55:11 by gstrauss          #+#    #+#             */
-/*   Updated: 2019/07/02 14:11:44 by gstrauss         ###   ########.fr       */
+/*   Updated: 2019/07/03 08:14:08 by gstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 100
+# define BUFF_SIZE 10
 
 # include <sys/types.h>
 # include <sys/stat.h>
